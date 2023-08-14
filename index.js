415348d6a308e4556013a69eb7f4d75050cca8bb
Hakizimana Fred <hakifred20@gmail.com>
2023-08-14 11:47:23 +0300

    removed console logs

---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+const wipeLogs = (inputFile, outpuFile) => {
+  try {
+    const data = fs.readFileSync(inputFile, "utf-8");
+    const lines = data.split("\n");
+    const filteredLines = lines.filter((line) => !line.includes("console.log"));
+
+    const cleanedContent = filteredLines.join("\n");
+
+    fs.writeFileSync(inputFile, cleanedContent, "utf8");
+    console.log("console.logs removed successfully.");
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+wipeLogs("sample.js");
---
+++ sample.js
@@ -0,0 +1,10 @@
+function testLogs() {
+  console.log("Starting............");
+  const a = 1;
+  console.log("variable a declared");
+  const b = 2;
+  console.log("variable a declared");
+
+  console.log("completed");
+  return a + b;
+}
